d7b71e4e15658f8c7f33dbff8c24909c354ba709
5647kr <5647kr@gmail.com>
2024-03-29 15:34:34 +0900

    코드 정리

--- index.js
+++ index.js
@@ -1,54 +1,103 @@
-const dataList = fetch("./index.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const bigMonData = data[0].bigMonster;
-    const typeListData = data[1].typeList;
-    const workListData = data[2].workList;
-    console.log(bigMonData)
-    return { bigMonData, typeListData, workListData };
+fetch("./index.json")
+  .then((response) => {
+    return response.json();
   })
-  .catch((error) => console.log(error));
+  .then((array) => {
+    Data(array);
+  });
 
-  dataList.then(({bigMonData}) => {
-    const monList = document.querySelector(".monster-list");
-  
-    bigMonData.forEach(item => {
-      console.log(item)
-      const monLi = document.createElement("li")
-      const monCard = document.createElement("div")
-      const monImg = document.createElement("img")
-      monImg.src = item.imgurl
-      const monName = document.createElement("strong")
-      monName.textContent = item.name
-      monCard.classList.add("monster-card");
+function Data(array) {
+  const monsterData = array[0].bigMonster;
+  const typeData = array[1].typeList;
+  const seriesData = array[2].workList;
+  const monsterList = document.querySelector(".monster-list");
+  const typeList = document.querySelector(".type-list");
+  const seriesList = document.querySelector(".work-list");
 
-      monList.appendChild(monLi);
-      monLi.appendChild(monCard);
-      monCard.appendChild(monImg);
-      monCard.appendChild(monName);
+  // element 생성 함수
+  function createLi() {
+    return document.createElement("li");
+  }
+  function createInput() {
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    return checkBox;
+  }
+  function createLabel() {
+    return document.createElement("label");
+  }
+
+  // 몬스터 카드 생성
+  monsterData.forEach((monster) => {
+    const cardItem = createLi();
+    const cardWrap = document.createElement("div");
+    cardWrap.classList.add("monster-card");
+
+    const cardContent = `
+      <img src="${monster.imgurl}">
+      <strong>${monster.name}</strong>
+    `;
+
+    cardWrap.innerHTML = cardContent;
+    cardItem.appendChild(cardWrap);
+    monsterList.appendChild(cardItem);
+
+    // 카드 상세 내용 마우스이벤트
+    cardWrap.addEventListener("mouseenter", () => {
+      const cardDetailWrap = document.createElement("div");
+      cardDetailWrap.classList.add("detail");
+      cardDetailWrap.innerHTML = `
+        <h3><span>종별: </span>${monster.type}</h3>
+        <h3><span>종: </span>${monster.speices}</h3>
+        <h3><span>이름: </span>${monster.name}</h3>
+        <h3><span>별명: </span>${monster.nickname}</h3>
+        <h3><span>등장 작품: </span>${monster.series}</h3>
+      `;
+      cardWrap.appendChild(cardDetailWrap);
+    });
+
+    cardWrap.addEventListener("mouseleave", () => {
+      const cardDetailWrap = cardWrap.querySelector(".detail");
+      cardWrap.removeChild(cardDetailWrap);
     });
   });
 
-dataList.then(({typeListData}) => {
-  const typeList = document.querySelector(".type-list");
+  // type radio 생성
+  typeData.forEach((type) => {
+    const typeItem = createLi();
 
-  typeListData.forEach(item => {
-    const typeLi = document.createElement("li");
-    const typeBtn = document.createElement("button");
-    typeBtn.textContent = item.type;
-    typeList.appendChild(typeLi);
-    typeLi.appendChild(typeBtn);
+    const typeCheckBox = createInput();
+    const checkBoxLabel = createLabel();
+
+    typeCheckBox.id = type.type;
+    typeCheckBox.checked = true;
+
+    checkBoxLabel.textContent = type.type;
+    checkBoxLabel.htmlFor = type.type;
+
+    typeList.appendChild(typeItem);
+    typeItem.appendChild(typeCheckBox);
+    typeItem.appendChild(checkBoxLabel);
   });
-});
 
-dataList.then(({workListData}) => {
-  const workList = document.querySelector(".work-list");
+  // series radio 생성
+  seriesData.forEach((series) => {
+    const seriesItem = createLi();
+    const seriesCheckBox = createInput();
+    const checkBoxLabel = createLabel();
+    const seriesAbbr = document.createElement("abbr");
 
-  workListData.forEach(item => {
-    const workLi = document.createElement("li");
-    const workBtn = document.createElement("button");
-    workBtn.textContent = item.work;
-    workList.appendChild(workLi);
-    workLi.appendChild(workBtn);
-  });
-});
+    seriesCheckBox.id = series.work;
+    seriesCheckBox.checked = true;
+
+    checkBoxLabel.htmlFor = series.work;
+
+    seriesAbbr.title = series.fullName;
+    seriesAbbr.textContent = series.work;
+
+    seriesList.appendChild(seriesItem);
+    seriesItem.appendChild(seriesCheckBox);
+    seriesItem.appendChild(checkBoxLabel);
+    checkBoxLabel.appendChild(seriesAbbr);
+  })
+}

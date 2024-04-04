44b4513791f96254f929e4c3009f12d2bb7c9f6d
5647kr <5647kr@gmail.com>
2024-04-04 14:54:28 +0900

    전체보기 버튼 하나로 통합

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
 fetch("./index.json")
   .then((response) => {
     return response.json();
@@ -13,9 +14,7 @@ function Data(array) {
   const monsterList = document.querySelector(".monster-list");
   const typeList = document.querySelector(".type-list");
   const seriesList = document.querySelector(".work-list");
-  const typeBtn = document.querySelector(".btn-list .typeBtn");
-  const seriesBtn = document.querySelector(".btn-list .seriesBtn")
-
+  const btnList = document.querySelector(".btn-list");
   // element 생성 함수
   function createLi() {
     return document.createElement("li");
@@ -33,7 +32,11 @@ function Data(array) {
   monsterData.forEach((monster) => {
     const cardItem = createLi();
     const cardWrap = document.createElement("div");
+    const cardInner = document.createElement("div");
+    const cardIntro = document.createElement("div");
     cardWrap.classList.add("monster-card");
+    cardInner.classList.add("monster-inner");
+    cardIntro.classList.add("monster-intro");
 
     const cardContent = `
     <img src="${monster.imgurl}">
@@ -42,29 +45,36 @@ function Data(array) {
     <p class="a11y-hidden" id="series">${monster.seriesId}</p>
     `;
 
-    cardWrap.innerHTML = cardContent;
+    const cardDetailWrap = document.createElement("div");
+    cardDetailWrap.classList.add("detail");
+
+    cardDetailWrap.innerHTML = `
+      <h3><span>종별: </span>${monster.type}</h3>
+      <h3><span>종: </span>${monster.speices}</h3>
+      <h3><span>이름: </span>${monster.name}</h3>
+      <h3><span>별명: </span>${monster.nickname}</h3>
+      <h3><span>등장 작품: </span>${monster.series}</h3>
+    `;
+    
+    cardIntro.innerHTML = cardContent;
+    cardInner.appendChild(cardIntro);
+    cardWrap.appendChild(cardInner);
     cardItem.appendChild(cardWrap);
     monsterList.appendChild(cardItem);
+    
+    cardInner.appendChild(cardDetailWrap);
 
-    // 카드 상세 내용 마우스이벤트
-    cardWrap.addEventListener("mouseenter", () => {
-      const cardDetailWrap = document.createElement("div");
-      cardDetailWrap.classList.add("detail");
-      cardDetailWrap.innerHTML = `
-        <h3><span>종별: </span>${monster.type}</h3>
-        <h3><span>종: </span>${monster.speices}</h3>
-        <h3><span>이름: </span>${monster.name}</h3>
-        <h3><span>별명: </span>${monster.nickname}</h3>
-        <h3><span>등장 작품: </span>${monster.series}</h3>
-      `;
-      cardWrap.appendChild(cardDetailWrap);
-    });
-
-    cardWrap.addEventListener("mouseleave", () => {
-      const cardDetailWrap = cardWrap.querySelector(".detail");
-      cardWrap.removeChild(cardDetailWrap);
-    });
+    // 카드 클릭 이벤트
+    cardWrap.addEventListener("click", () => {
+      cardWrap.classList.toggle("flipped")
+    })
   });
+
+  //! card 초기설정 삭제 가능성
+  const cards = document.querySelectorAll(".monster-list li");
+  cards.forEach((card) => {
+    card.style.display = "none"
+  })
 
   // type checkbox 생성
   typeData.forEach((type) => {
@@ -103,57 +113,123 @@ function Data(array) {
     checkBoxLabel.appendChild(seriesAbbr);
   });
 
-  // all btn 생성
-  const typeAllBtnItem = createLi();
-  const typeResetBtnItem = createLi();
-  const typeAllBtn = document.createElement("button");
-  const typeResetBtn = document.createElement("button");
+  // typebtn 생성
+  const typeBtn = document.createElement("button");
+  btnList.appendChild(typeBtn);
+  typeBtn.value = "true";
+  typeBtn.textContent = "전체 해제";
 
-  typeAllBtnItem.appendChild(typeAllBtn);
-  typeResetBtnItem.appendChild(typeResetBtn);
-  typeBtn.appendChild(typeAllBtnItem);
-  typeBtn.appendChild(typeResetBtnItem);
+  const seriesBtn = document.createElement("button");
+  btnList.appendChild(seriesBtn);
+  seriesBtn.value = "true";
+  seriesBtn.textContent = "전체 선택";
+  seriesBtn.disabled = true;
 
-  typeAllBtn.textContent = "전체선택";
-  typeResetBtn.textContent = "전체해제";
+  typeBtn.addEventListener("click", () => {
+    if (typeBtn.value === "true") {
+      typeBtn.value = "false";
+      typeBtn.textContent = "전체 선택";
 
-  const seriesAllBtnItem = createLi();
-  const seriesResetBtnItem = createLi();
-  const seriesAllBtn = document.createElement("button");
-  const seriesResetBtn = document.createElement("button");
+      typeCheck.forEach((checkbox) => {
+        checkbox.checked = false;
+      });
 
-  seriesBtn.appendChild(seriesAllBtnItem);
-  seriesBtn.appendChild(seriesResetBtnItem);
-  seriesAllBtnItem.appendChild(seriesAllBtn);
-  seriesResetBtnItem.appendChild(seriesResetBtn);
+      cards.forEach((card) => {
+        card.style.display = "none";
+      });
+    } else {
+      typeBtn.value = "true";
+      typeBtn.textContent = "전체 해제";
 
-  seriesAllBtn.textContent = "전체선택";
-  seriesResetBtn.textContent = "전체해제";
-  
-  //! seriesAllBtn disabled 나중에 풀것!
-  seriesAllBtn.disabled = true;
+      typeCheck.forEach((checkbox) => {
+        checkbox.checked = true;
+      });
 
-  seriesResetBtn.disabled = true;
+      seriesBtn.value = "true";
+      seriesBtn.textContent = "전체 선택";
 
-  // card 초기설정
-  const cards = document.querySelectorAll(".monster-list li");
+      seriesCheck.forEach((checkbox) => {
+        checkbox.checked = false;
+      });
+
+      cards.forEach((card) => {
+        card.style.display = "block";
+      });
+    }
+  });
+
+  // seriesBtn 생성
+  seriesBtn.addEventListener("click", () => {
+    if (seriesBtn.value === "true") {
+      seriesBtn.value = "false";
+      seriesBtn.textContent = "전체 해제";
+
+      seriesCheck.forEach((checkbox) => {
+        checkbox.checked = true;
+      });
+
+      typeBtn.value = "false";
+      typeBtn.textContent = "전체 선택";
+
+      typeCheck.forEach((checkbox) => {
+        checkbox.checked = false;
+      });
+
+      cards.forEach((card) => {
+        card.style.display = "block";
+      });
+    } else {
+      seriesBtn.value = "true";
+      seriesBtn.textContent = "전체 선택";
+
+      seriesCheck.forEach((checkbox) => {
+        checkbox.checked = false;
+      });
+
+      typeCheck.forEach((checkbox) => {
+        checkbox.checked = false;
+      })
+
+      cards.forEach((card) => {
+        card.style.display = "none";
+      });
+    }
+  });
+
+
+  // 체크박스와 카드 종류별 선언문
+  const typeCheck = document.querySelectorAll(".type-list input");
+  const cardType = document.querySelectorAll(
+    ".monster-list li .monster-card #type"
+    );
+    
+  const seriesCheck = document.querySelectorAll(".work-list input");
+  const cardSeries = document.querySelectorAll(
+      ".monster-list li .monster-card #series"
+      );
   
   // type 필터링 기능
-  const typeCheck = document.querySelectorAll(".type-list input");
-
   typeCheck.forEach((checkbox) => {
     checkbox.checked = true;
+
     checkbox.addEventListener("click", (e) => {
-      const seriesCardsReset = () => {
-        cards.forEach((card) => {
-          card.style.display = "none"
-        })
+      // 체크박스 하나라도 checked가 아닌 경우
+      if (!checkbox.checked) {
+        typeBtn.value = "false";
+        typeBtn.textContent = "전체 선택";
+      } else {
+        typeBtn.value = "true";
+        typeBtn.textContent = "전체 해제"
       }
 
-      const cardType = document.querySelectorAll(
-        ".monster-list li .monster-card #type"
-      );
+      // 종별 체크박스 하나라도 checked인 경우
+      seriesCheck.forEach((checkbox) => {
+        checkbox.checked = false;
+      });
+      seriesBtn.value = "true";
+      seriesBtn.textContent = "전체 선택";
 
+      // 종별 필터링 기능
       cardType.forEach((card) => {
         const typeCheckList = card.closest("li");
 
@@ -161,119 +237,50 @@ function Data(array) {
           typeCheckList.style.display = "block";
         } else if (!checkbox.checked && e.target.id === card.textContent) {
           typeCheckList.style.display = "none";
-        }
+        } 
       });
-      checkbox.removeEventListener("click", seriesCardsReset)
     });
-
-    typeResetBtn.addEventListener("click", () => {
-      checkbox.checked = false;
-      const cards = document.querySelectorAll(".monster-list li");
-      cards.forEach((card) => {
-        card.style.display = "none";
-      })
-    })
-
-    typeAllBtn.addEventListener("click", () => {
-      checkbox.checked = true;
-      const cards = document.querySelectorAll(".monster-list li");
-      cards.forEach((card) => {
-        card.style.display = "block";
-      })
-    })
   });
 
   // series 필터링 기능
-  const seriesCheck = document.querySelectorAll(".work-list input");
-  const cardSeries = document.querySelectorAll(
-    ".monster-list li .monster-card #series"
-  );
-
 
   seriesCheck.forEach((checkbox) => {
-    //! checkbox 비활성화 풀것!
     checkbox.disabled = true;
-    checkbox.addEventListener("click", (e) => {
-      const typeCardsReset = () => {
-        cards.forEach((card) => {
-          card.style.display = "none"
-        })
+
+    checkbox.addEventListener("change", () => {
+      // 체크박스 하나라도 checked가 아닌 경우
+      if (!checkbox.checked) {
+        seriesBtn.value = "true";
+        seriesBtn.textContent = "전체 선택";
+      } else {
+        seriesBtn.value = "false";
+        seriesBtn.textContent = "전체 해제"
       }
+
+      // 타입체크박스 checked 해제
+      typeCheck.forEach((checkbox) => {
+        checkbox.checked = false;
+      });
+
+      // 타입버튼 초기화
+      typeBtn.value = "false";
+      typeBtn.textContent = "전체 선택";
+
 
       cardSeries.forEach((card) => {
         const seriesCheckList = card.closest("li");
 
-        if (e.target.checked && card.textContent.includes(e.target.id)) {
-          seriesCheckList.style.display = "block"
-        } else if(!e.target.checked && card.textContent.includes(e.target.id)) {
-          seriesCheckList.style.display = "none"
-        }
+        seriesCheck.forEach((checkbox) => {
+        });
       });
-      checkbox.removeEventListener("click", typeCardsReset)
     });
-
-
-    seriesResetBtn.addEventListener("click", () => {
-      checkbox.checked = false;
-      const cards = document.querySelectorAll(".monster-list li");
-      cards.forEach((card) => {
-        card.style.display = "none";
-      })
-    })
-
-    seriesAllBtn.addEventListener("click", () => {
-      checkbox.checked = true;
-      const cards = document.querySelectorAll(".monster-list li");
-      cards.forEach((card) => {
-        card.style.display = "block";
-      })
-    })
-  })
-
-
-  // 체크박스 리셋하기
-  typeCheck.forEach((checkbox) => {
-    checkbox.addEventListener("click", () => {
-      seriesCheck.forEach((uncheckedbox) => {
-        uncheckedbox.checked = false;
-      })
-      typeResetBtn.disabled = false;
-    })
-
-    typeAllBtn.addEventListener("click", () => {
-      seriesCheck.forEach((uncheckedbox) => {
-        uncheckedbox.checked = false;
-      })
-      typeResetBtn.disabled = false;
-    })
-
-    typeResetBtn.addEventListener("click", () => {
-      typeResetBtn.disabled = true;
-    })
-  })
-
-  seriesCheck.forEach((checkbox) => {
-    checkbox.addEventListener("click", () => {
-      typeCheck.forEach((uncheckedbox) => {
-        uncheckedbox.checked = false;
-      })
-      seriesResetBtn.disabled = false;
-    })
-    seriesAllBtn.addEventListener("click", () => {
-      typeCheck.forEach((uncheckedbox) => {
-        uncheckedbox.checked = false;
-      })
-      seriesResetBtn.disabled = false;
-    })
-    seriesResetBtn.addEventListener("click", () => {
-      seriesResetBtn.disabled = true;
-    })
-  })
-
-  seriesAllBtn.addEventListener("click", () => {
-    typeResetBtn.disabled = true;
-  })
-  typeAllBtn.addEventListener("click", () => {
-    seriesResetBtn.disabled = true;
+  });
+  cards.forEach((card) => {
+    card.style.display  = "block"
   })
 }
+})
+
+//! 문제점1: 시리즈 누르고 다시 종별 checkbox누르면 시리즈필터링한  카드가 그대로 있음
+
+//! 문제점2: 시리즈 전체 선택한 뒤 시리즈 체크박스 하나만 눌러도 카드가 전부다 사라짐

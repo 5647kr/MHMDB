929cad2df30bebcd76245195073c337cd0a618ae
5647kr <5647kr@gmail.com>
2024-05-31 21:46:31 +0900

    데이터 구조화

---
+++ structured-data.js
@@ -0,0 +1,11 @@
+{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "url": "http://www.mhmdb.info/",
+  "name": "몬스터 헌터 몬스터 데이터베이스",
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": "http://www.mhmdb.info/?s={search_term_string}",
+    "query-input": "required name=search_term_string"
+  }
+}

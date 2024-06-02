ea1510e04d2892609e09331fbfb3964dfc801234
5647kr <5647kr@gmail.com>
2024-06-02 23:14:15 +0900

    데이터 마크업 수정

--- structured-data.js
+++ structured-data.js
@@ -1,11 +1,12 @@
 {
   "@context": "https://schema.org",
   "@type": "WebSite",
-  "url": "http://www.mhmdb.info/",
+  "url": "http://mhmdb.info/",
   "name": "몬스터 헌터 몬스터 데이터베이스",
+  "description": "MHMDB는 몬스터 헌터 시리즈의 모든 대형 몬스터 정보를 제공합니다.",
   "potentialAction": {
     "@type": "SearchAction",
-    "target": "http://www.mhmdb.info/?s={search_term_string}",
+    "target": "http://mhmdb.info/?s={search_term_string}",
     "query-input": "required name=search_term_string"
   }
 }

79969df99bcb6922ded5b5d1fc0254fefbdb587c
5647kr <5647kr@gmail.com>
2024-07-27 16:07:08 +0900

    [🔨 refactor] scroll 여부에 따른 스타일 적용 삭제

--- javascript/index.js
+++ javascript/index.js
@@ -8,27 +8,6 @@ const mainPageEvent = new MainPageEvent();
 dataGenerator.setup();
 // /module화
 
-// wrap 스크롤에 따라 width 값 조정
-function pageScroll() {
-  const wrapElements = document.querySelectorAll(".wrap");
-  const hasScroll = window.innerWidth > document.documentElement.clientWidth;
-
-  if (hasScroll) {
-    wrapElements.forEach((wrap) => {
-      wrap.classList.add('scroll');
-    });
-  } else {
-    wrapElements.forEach((wrap) => {
-      wrap.classList.remove('scroll');
-    });
-  }
-}
-
-document.addEventListener("DOMContentLoaded", pageScroll);
-
-window.addEventListener('resize', pageScroll);
-// /wrap 스크롤에 따라 width 값 조정
-
 
 // url 설정
 
